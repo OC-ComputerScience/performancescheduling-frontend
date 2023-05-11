283e0b277795bb3efae4236522fc4faefed2d04a
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-05-11 15:41:40 -0500

    updated colors and menu bar

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <script setup>
-import ocLogo from "../../public/oc_logo_social.png";
+import ocLogo from "../../public/oc_logo_red.png";
 import AuthServices from "../services/authServices.js";
 import { useLoginStore } from "../stores/LoginStore.js";
 import { ref, onMounted, watch } from "vue";
@@ -9,11 +9,16 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 const loginStore = useLoginStore();
 const drawer = ref(false);
-const title = "OC Music Department";
+const title = "PERFORMANCE SCHEDULING";
 const initials = ref("");
 const name = ref("");
 const activeMenus = ref([]);
 const menus = [
+  {
+    link: "studentHome",
+    text: "Home",
+    roles: [1],
+  },
   {
     link: "studentRepertoire",
     text: "Repertoire",
@@ -30,6 +35,11 @@ const menus = [
     roles: [1],
   },
   {
+    link: "facultyHome",
+    text: "Home",
+    roles: [2, 4],
+  },
+  {
     link: "facultyViewCritiques",
     text: "View Student Critiques",
     roles: [2],
@@ -38,6 +48,11 @@ const menus = [
     link: "createAvailability",
     text: "Event Availability",
     roles: [2, 4],
+  },
+  {
+    link: "adminHome",
+    text: "Home",
+    roles: [3],
   },
   {
     link: "adminViewUsers",
@@ -52,6 +67,30 @@ const menus = [
 ];
 const userRoles = ref([]);
 const { currentRole } = storeToRefs(loginStore);
+const activeLink = ref("");
+const roleColors = [
+  {
+    roleId: 1,
+    backgroundColor: "ourLightMaroon",
+    textColor: "ourMaroon",
+  },
+  {
+    roleId: 2,
+    backgroundColor: "ourLightDarkBlue",
+    textColor: "ourDarkBlue",
+  },
+  {
+    roleId: 3,
+    backgroundColor: "ourLightOrange",
+    textColor: "ourOrange",
+  },
+  {
+    roleId: 4,
+    backgroundColor: "ourLightTeal",
+    textColor: "ourTeal",
+  },
+];
+const roleColor = ref({});
 
 function getUserRoles() {
   userRoles.value = [];
@@ -70,6 +109,7 @@ function goToHome() {
   } else {
     router.push({ path: "base" });
   }
+  updateActiveLink();
 }
 function resetMenu() {
   if (loginStore.user != null) {
@@ -98,10 +138,21 @@ function logout() {
 }
 function changeComp(route) {
   router.push({ path: route });
+  updateActiveLink();
 }
+function updateActiveLink() {
+  setTimeout(function () {
+    activeLink.value = router.currentRoute.value.name;
+    console.log(activeLink.value);
+  }, 1);
+}
+function changeRole() {}
 
 watch(currentRole, () => {
   if (currentRole != {}) {
+    roleColor.value = roleColors.find((obj) => {
+      return obj.roleId == loginStore.currentRole.roleId;
+    });
     goToHome();
     resetMenu();
   }
@@ -110,13 +161,16 @@ watch(currentRole, () => {
 onMounted(() => {
   getUserRoles();
   resetMenu();
+  updateActiveLink();
+  roleColor.value = roleColors.find((obj) => {
+    return obj.roleId == loginStore.currentRole.roleId;
+  });
 });
 </script>
 <template>
-  <v-app-bar color="maroon" prominent class="elevation-0">
-    <template v-slot:prepend>
-      <!-- OC logo image -->
-      <v-btn icon plain @click="goToHome">
+  <v-container fluid class="mx-3 my-1">
+    <v-row>
+      <v-btn icon plain elevation="0" @click="goToHome">
         <v-img
           class="mx-2"
           :src="ocLogo"
@@ -126,128 +180,169 @@ onMounted(() => {
           transition="scale-transition"
         ></v-img>
       </v-btn>
-    </template>
-    <!-- OC Music Dept title in top bar -->
-    <v-toolbar-title class="title">
-      {{ title }}
-    </v-toolbar-title>
-    <!-- <v-spacer></v-spacer> -->
-    <div v-if="userRoles.length > 1" class="align-right justify-right d-flex">
-      <v-toolbar-items
-        v-for="menu in activeMenus"
-        :key="menu.link"
-        class="hidden-md-and-down"
-      >
-        <v-btn
-          class="hidden-md-and-down white--text mx-1"
-          exact
-          text
-          @click="changeComp(menu.link)"
+      <strong :style="{ color: 'maroon' }" class="mt-3 ml-3">{{
+        title
+      }}</strong>
+      <v-spacer></v-spacer>
+      <div class="align-right justify-right d-flex">
+        <v-toolbar-items
+          v-for="menu in activeMenus"
+          :key="menu.link"
+          class="hidden-md-and-down"
+          :class="activeLink === menu.link ? 'text-ourBlue' : 'text-ourMaroon'"
         >
-          {{ menu.text }}
-        </v-btn>
-      </v-toolbar-items>
-      <v-btn
-        v-if="loginStore.currentRole.role === ''"
-        variant="text"
-        @click="logout()"
-      >
-        Log back in
-      </v-btn>
-      <v-select
-        :items="userRoles"
-        item-title="role"
-        v-model="loginStore.currentRole"
-        variant="underlined"
-        class="ma-2"
-        return-object
-      >
-      </v-select>
-    </div>
-    <div v-else class="align-right justify-right d-flex">
-      <v-toolbar-items
-        v-for="menu in activeMenus"
-        :key="menu.link"
-        class="hidden-md-and-down"
-      >
+          <v-btn
+            class="hidden-md-and-down mx-1 menu-link font-weight-bold"
+            exact
+            text
+            :ripple="false"
+            @click="changeComp(menu.link)"
+          >
+            {{ menu.text }}
+          </v-btn>
+        </v-toolbar-items>
         <v-btn
-          class="hidden-md-and-down white--text mx-1"
-          style="height: 45px"
-          exact
-          text
-          @click="changeComp(menu.link)"
+          v-if="loginStore.currentRole.role === ''"
+          variant="text"
+          @click="logout()"
         >
-          {{ menu.text }}
+          Log back in
         </v-btn>
-      </v-toolbar-items>
-      <v-btn
-        v-if="loginStore.currentRole.role === ''"
-        variant="text"
-        @click="logout()"
+      </div>
+      <v-icon class="mt-3 mr-5" color="ourMaroon" icon="mdi-bell"></v-icon>
+      <v-menu
+        bottom
+        min-width="250px"
+        rounded
+        offset-y
+        v-if="loginStore.user != null"
       >
-        Log back in
-      </v-btn>
-    </div>
-
-    <v-menu
-      bottom
-      min-width="250px"
-      rounded
-      offset-y
-      v-if="loginStore.user != null"
-    >
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" icon x-large>
-          <v-avatar v-if="loginStore.user != null" color="secondary">
-            <v-img
-              v-if="loginStore.user.picture"
-              :src="loginStore.user.picture"
-            ></v-img>
-            <span v-else class="accent--text font-weight-bold">{{
-              initials
-            }}</span>
-          </v-avatar>
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-text>
-          <div class="mx-auto text-center">
-            <v-avatar color="secondary" class="mt-2 mb-2">
-              <img
-                v-if="loginStore.user.picture"
-                :src="loginStore.user.picture"
-                referrerpolicy="no-referrer"
-              />
-              <span v-else class="accent--text font-weight-bold">{{
-                initials
-              }}</span>
-            </v-avatar>
-            <h3>{{ name }}</h3>
-            <p class="text-caption mt-1">
-              {{ loginStore.user.email }}
-            </p>
-            <v-divider class="my-3"></v-divider>
-            <v-btn variant="text" @click="changeComp('studentSettings')">
-              Student Settings
-            </v-btn>
-            <br />
-            <v-btn variant="text" @click="logout()"> Logout </v-btn>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-menu>
-    <v-app-bar-nav-icon
-      dark
-      class="hidden-lg-and-up"
-      @click="drawer = !drawer"
-    ></v-app-bar-nav-icon>
-  </v-app-bar>
+        <template v-slot:activator="{ props }">
+          <v-btn
+            :color="roleColor.backgroundColor"
+            :class="'font-weight-bold text-' + roleColor.textColor"
+            height="50"
+            v-bind="props"
+            x-large
+            elevation="0"
+          >
+            {{ loginStore.currentRole.role }}
+            <template v-slot:append>
+              <v-avatar
+                v-if="loginStore.user != null"
+                color="secondary"
+                class="with-outline"
+              >
+                <v-img
+                  v-if="loginStore.user.picture"
+                  :src="loginStore.user.picture"
+                ></v-img>
+                <span v-else class="accent--text font-weight-bold">{{
+                  initials
+                }}</span>
+              </v-avatar>
+            </template>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-text>
+            <div class="mx-auto">
+              <h3>{{ name }}</h3>
+              <p class="text-caption text-grey">
+                {{ loginStore.user.email }}
+              </p>
+              <v-btn
+                color="ourLightMaroon"
+                :class="'font-weight-bold text-ourMaroon mt-2'"
+                density="comfortable"
+                elevation="0"
+                width="250px"
+                @click="changeComp('studentSettings')"
+              >
+                Switch to Student View
+              </v-btn>
+              <br />
+              <v-btn
+                color="ourLightDarkBlue"
+                :class="'font-weight-bold text-ourDarkBlue mt-2'"
+                density="comfortable"
+                elevation="0"
+                width="250px"
+                @click="changeComp('studentSettings')"
+              >
+                Switch to Faculty View
+              </v-btn>
+              <br />
+              <v-btn
+                color="ourLightOrange"
+                :class="'font-weight-bold text-ourOrange mt-2'"
+                density="comfortable"
+                elevation="0"
+                width="250px"
+                @click="changeComp('studentSettings')"
+              >
+                Switch to Admin View
+              </v-btn>
+              <br />
+              <v-btn
+                color="ourLightTeal"
+                :class="'font-weight-bold text-ourTeal mt-2'"
+                density="comfortable"
+                elevation="0"
+                width="250px"
+                @click="changeComp('studentSettings')"
+              >
+                Switch to Accompanist View
+              </v-btn>
+              <br />
+              <v-btn
+                prepend-icon="mdi-cog"
+                color="ourLightBlue"
+                :class="'mt-2 menu-button font-weight-bold text-ourBlue'"
+                density="comfortable"
+                elevation="0"
+                width="250px"
+                @click="changeComp('studentSettings')"
+              >
+                <template v-slot:prepend>
+                  <v-icon></v-icon>
+                </template>
+                User Settings
+              </v-btn>
+              <br />
+              <v-btn
+                prepend-icon="mdi-logout"
+                color="ourMediumGray"
+                :class="'mt-2 menu-button font-weight-bold text-ourLightGray'"
+                density="comfortable"
+                elevation="0"
+                width="250px"
+                @click="logout()"
+              >
+                <template v-slot:prepend>
+                  <v-icon></v-icon>
+                </template>
+                Logout
+              </v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-menu>
+      <v-icon
+        class="hidden-lg-and-up mt-3 ml-2"
+        icon="mdi-menu"
+        color="ourMaroon"
+        @click="drawer = !drawer"
+      >
+      </v-icon>
+    </v-row>
+  </v-container>
   <v-navigation-drawer
     v-model="drawer"
     class="hidden-lg-and-up"
     app
     location="right"
-    color="primary"
+    color="white"
   >
     <v-list>
       <v-list-item
@@ -255,6 +350,7 @@ onMounted(() => {
         :key="menu.text"
         exact
         @click="changeComp(menu.link)"
+        :class="activeLink === menu.link ? 'text-ourBlue' : 'text-maroon'"
       >
         <v-list-item-title class="white--text"
           >{{ menu.text }}
@@ -266,5 +362,15 @@ onMounted(() => {
 <style>
 .text-center {
   text-align: center;
+}
+.menu-link {
+  font-size: 15px;
+}
+.with-outline {
+  border: 2px solid white;
+}
+.menu-button {
+  justify-content: flex-start;
+  text-align: left;
 }
 </style>
