d81b13de0d28d90e1f503efeda93c672004b3297
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-05-16 12:18:17 -0500

    added navigation to studentPerformances page

---
+++ src/components/student/StudentPerformances.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useLoginStore } from "../stores/LoginStore.js";
+import { ref, onMounted } from "vue";
+
+const loginStore = useLoginStore();
+
+onMounted(() => {});
+</script>
+<template></template>
+<style></style>
