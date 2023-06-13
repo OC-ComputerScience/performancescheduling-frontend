f72ba9d6f20899e2b609765c11d4abb87b7b8baa
ethanimooney <ethanimooney@gmail.com>
2023-06-13 13:24:20 -0500

    Remove excess code

--- src/components/NotificationItem.vue
+++ src/components/NotificationItem.vue
@@ -1,22 +1,16 @@
 <script setup>
-import { ref, onMounted } from "vue";
-
 const props = defineProps({
   notificationData: { type: [Object], required: true },
-});
-
-onMounted(() => {
-  console.log(props.notificationData);
 });
 </script>
 
 <template>
   <v-card flat class="flatCardBorder bg-lightGray mt-4">
     <v-card-title class="font-weight-semi-bold pb-0 text-blue">
-      {{ notificationData.title }}
+      {{ props.notificationData.title }}
     </v-card-title>
     <v-card-text class="pb-0">
-      {{ notificationData.body }}
+      {{ props.notificationData.body }}
     </v-card-text>
     <v-card-actions class="pt-0">
       <!-- TODO: Add notificationData.link to this -->
@@ -25,7 +19,7 @@ onMounted(() => {
         size="small"
         class="font-weight-semi-bold ml-2 bg-blue text-none py-0"
       >
-        View {{ notificationData.type }}
+        View {{ props.notificationData.type }}
       </v-btn>
     </v-card-actions>
   </v-card>
--- src/components/student/InstrumentItem.vue
+++ src/components/student/InstrumentItem.vue
@@ -1,22 +1,13 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { useLoginStore } from "../../stores/LoginStore.js";
-
-const loginStore = useLoginStore();
-
 const props = defineProps({
   instrumentData: { type: [Object], required: true },
-});
-
-onMounted(() => {
-  console.log(props.instrumentData);
 });
 </script>
 
 <template>
   <v-card flat class="flatCardBorder bg-lightDarkBlue mt-4">
     <v-card-title class="font-weight-semi-bold text-darkBlue">
-      {{ instrumentData.name }}
+      {{ props.instrumentData.name }}
     </v-card-title>
   </v-card>
 </template>
