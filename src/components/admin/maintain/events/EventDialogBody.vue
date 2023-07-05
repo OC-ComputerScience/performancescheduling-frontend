65be34a1565d272ab00f975d255e030d909eca9e
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-07-04 20:36:07 -0500

    dialog body prep

--- src/components/admin/maintain/events/EventDialogBody.vue
+++ src/components/admin/maintain/events/EventDialogBody.vue
@@ -1,48 +1,73 @@
 <script setup>
 import { ref, onMounted } from "vue";
-
+import EventDataService from "../../../../services/EventDataService";
+import EventTypeDataService from "../../../../services/EventTypeDataService";
 import SemesterDataService from "../../../../services/SemesterDataService";
+import LocationDataService from "../../../../services/LocationDataService";
 import { formatDate } from "../../../../composables/dateFormatter";
 
 const emits = defineEmits([
-  "addSemesterSuccessEvent",
-  "closeAddSemesterDialogEvent",
-  "closeSemesterDialogEvent",
-  "updateSemesterSuccessEvent",
-  "disableSemesterEvent",
-  "enableSemesterEvent",
+  "addEventSuccessEvent",
+  "closeAddEventDialogEvent",
+  "closeEventDialogEvent",
+  "updateEventSuccessEvent",
+  "disableEventEvent",
+  "enableEventEvent",
 ]);
 
 const props = defineProps({
-  semesterData: { type: [Object], required: true },
+  eventData: { type: [Object], required: true },
   isEdit: { type: [Boolean], required: true },
 });
 const form = ref(null);
-const editedSemesterData = ref(props.semesterData);
+const editedEventData = ref(Object.assign({}, props.eventData));
+const semesters = ref([]);
+const eventTypes = ref([]);
+const locations = ref([]);
 
-// const editedSemesterRoles = ref(props.semesterRoles);
-
-onMounted(() => {
+onMounted(async () => {
   if (props.isEdit) {
-    editedSemesterData.value["id"] = props.semesterData.id;
-    editedSemesterData.value.startDate = formatDate(
-      editedSemesterData.value.startDate
-    );
-    editedSemesterData.value.endDate = formatDate(
-      editedSemesterData.value.endDate
-    );
+    editedEventData.value.id = props.eventData.id;
+    editedEventData.value.date = formatDate(editedEventData.value.date);
   } else {
-    editedSemesterData.value["status"] = "Active";
+    editedEventData.value.status = "Active";
+    editedEventData.value.isReady = false;
   }
+  await getData();
 });
 
-async function addSemester() {
+async function getData() {
+  await EventTypeDataService.getAll("name")
+    .then((response) => {
+      eventTypes.value = response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  await SemesterDataService.getAll("name", false)
+    .then((response) => {
+      semesters.value = response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  await LocationDataService.getAll("roomName")
+    .then((response) => {
+      locations.value = response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+async function addEvent() {
   form.value.validate().then(async (valid) => {
-    console.log(valid.valid);
     if (valid.valid) {
-      await SemesterDataService.create(editedSemesterData.value)
+      await EventDataService.create(editedEventData.value)
         .then(async () => {
-          emits("addSemesterSuccessEvent");
+          emits("addEventSuccessEvent");
         })
         .catch((err) => {
           console.log(err);
@@ -51,15 +76,14 @@ async function addSemester() {
   });
 }
 
-async function updateSemester() {
+async function updateEvent() {
   await form.value.validate().then(async (valid) => {
     if (valid.valid) {
-      delete editedSemesterData.value["createdAt"];
-      delete editedSemesterData.value["updatedAt"];
+      delete editedEventData.value["updatedAt"];
 
-      await SemesterDataService.update(editedSemesterData.value)
+      await EventDataService.update(editedEventData.value)
         .then(() => {
-          emits("updateSemesterSuccessEvent");
+          emits("updateEventSuccessEvent");
         })
         .catch((err) => {
           console.log(err);
@@ -70,19 +94,19 @@ async function updateSemester() {
 
 function semesterNameCheck() {
   const pattern = /[0-9]{4}-[A-Z]{2}/;
-  return pattern.test(editedSemesterData.value.name)
+  return pattern.test(editedEventData.value.name)
     ? true
     : "Semester name must be in the format of YYYY-SS.";
 }
-function startDateCheck() {
+function dateCheck(date) {
   const pattern = new RegExp("[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$");
-  return pattern.test(editedSemesterData.value.startDate)
+  return pattern.test(date)
     ? true
     : "Date  must be in the format of MM/DD/YYYY.";
 }
 function endDateCheck() {
   const pattern = new RegExp("[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$");
-  return pattern.test(editedSemesterData.value.endDate)
+  return pattern.test(editedEventData.value.endDate)
     ? true
     : "Date must be in the format of MM/DD/YYYY.";
 }
@@ -97,7 +121,7 @@ function endDateCheck() {
             cols="auto"
             class="pt-0 mt-0 text-maroon font-weight-bold text-h4"
           >
-            {{ props.isEdit ? "Edit" : "Add" }} Semester
+            {{ props.isEdit ? "Edit" : "Add" }} Event
           </v-col>
         </v-row>
       </v-card-title>
@@ -105,7 +129,7 @@ function endDateCheck() {
         <v-row v-if="props.isEdit" class="pt-0 mt-0">
           <v-col cols="auto" class="pl-6" align-self="center">
             <v-card-title class="font-weight-bold text-darkBlue py-0 my-0">
-              {{ semesterData.name }}
+              {{ eventData.name }}
             </v-card-title>
           </v-col>
           <v-col v-if="props.isEdit" cols="auto" align-self="center">
@@ -114,11 +138,9 @@ function endDateCheck() {
               flat
               size="small"
               class="font-weight-bold mt-0 text-none text-white flatChipBorder"
-              :class="
-                semesterData.status === 'Active' ? 'bg-teal' : 'bg-maroon'
-              "
+              :class="eventData.isReady ? 'bg-teal' : 'bg-maroon'"
             >
-              {{ semesterData.status }}
+              {{ eventData.isReady ? "Ready" : "Not Ready" }}
             </v-chip>
           </v-col>
         </v-row>
@@ -128,16 +150,16 @@ function endDateCheck() {
           <v-card-subtitle
             class="pl-0 pb-2 font-weight-semi-bold text-darkBlue"
           >
-            Semester Name
+            Event Name
           </v-card-subtitle>
 
           <v-text-field
             placeholder="2000-FA"
-            v-model="editedSemesterData.name"
+            v-model="editedEventData.name"
             variant="plain"
             class="bg-lightGray text-blue font-weight-bold flatCardBorder pl-4 py-0 my-0 mb-4"
             :rules="[
-              () => !!editedSemesterData.name || 'This field is required',
+              () => !!editedEventData.name || 'This field is required',
               semesterNameCheck,
             ]"
           ></v-text-field>
@@ -149,11 +171,11 @@ function endDateCheck() {
           </v-card-subtitle>
           <v-text-field
             placeholder="MM/DD/YYYY"
-            v-model="editedSemesterData.startDate"
+            v-model="editedEventData.startDate"
             variant="plain"
             class="bg-lightGray text-blue font-weight-bold flatCardBorder pl-4 py-0 my-0 mb-4"
             :rules="[
-              () => !!editedSemesterData.startDate || 'This field is required',
+              () => !!editedEventData.startDate || 'This field is required',
               startDateCheck,
             ]"
           ></v-text-field>
@@ -164,11 +186,11 @@ function endDateCheck() {
           </v-card-subtitle>
           <v-text-field
             placeholder="MM/DD/YYYY"
-            v-model="editedSemesterData.endDate"
+            v-model="editedEventData.endDate"
             variant="plain"
             class="bg-lightGray text-blue font-weight-bold flatCardBorder pl-4 py-0 my-0 mb-4"
             :rules="[
-              () => !!editedSemesterData.endDate || 'This field is required',
+              () => !!editedEventData.endDate || 'This field is required',
               endDateCheck,
             ]"
           ></v-text-field>
@@ -178,7 +200,7 @@ function endDateCheck() {
         <v-btn
           flat
           class="font-weight-semi-bold mt-0 ml-auto text-none text-white bg-teal flatChipBorder"
-          @click="props.isEdit ? updateSemester() : addSemester()"
+          @click="props.isEdit ? updateEvent() : addEvent()"
         >
           {{ props.isEdit ? "Save" : "Add" }}
         </v-btn>
@@ -188,8 +210,8 @@ function endDateCheck() {
           :class="props.isEdit ? '' : 'mr-auto'"
           @click="
             props.isEdit
-              ? emits('closeSemesterDialogEvent')
-              : emits('closeAddSemesterDialogEvent')
+              ? emits('closeEventDialogEvent')
+              : emits('closeAddEventDialogEvent')
           "
         >
           Cancel
@@ -198,5 +220,3 @@ function endDateCheck() {
     </v-card>
   </v-form>
 </template>
-
-<style scoped></style>
