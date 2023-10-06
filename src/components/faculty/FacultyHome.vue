5be7679fe8c86fcc9011f398a60bbd818120d81f
dgriff24 <daniel.griffin24@gmail.com>
2023-10-06 16:05:01 -0500

    Now also sorts by start time

--- src/components/faculty/FacultyHome.vue
+++ src/components/faculty/FacultyHome.vue
@@ -76,7 +76,7 @@ async function retrieveData() {
       console.log(e);
     });
 
-  await EventDataService.getGTEDateForFaculty(new Date(), 'date')
+  await EventDataService.getGTEDateForFaculty(new Date(), 'date,startTime')
     .then((response) => {
       upcomingEvents.value = response.data;
     })
