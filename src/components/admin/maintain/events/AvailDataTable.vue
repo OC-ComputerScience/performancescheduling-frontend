77393925ddf9809c9f80d16d188c7680481e9213
David North <david.north@oc.edu>
2023-12-19 15:49:07 -0600

    added View Signup for Faculty/Accop

--- src/components/admin/maintain/events/AvailDataTable.vue
+++ src/components/admin/maintain/events/AvailDataTable.vue
@@ -1,66 +1,46 @@
 <script setup>
 const props = defineProps({
-    dataTableData: { type: [Object], required: true },
+  dataTableData: { type: [Object], required: true },
 });
 </script>
 
 <template>
-    <v-table>
-        <thead>
-            <tr>
-                <th class="text-left">
-                    Time slot
-                </th>
-                <th>
-                    |
-                </th>
-                <th class="text-left">
-                    Faculty
-                </th>
-                <th>
-                    |
-                </th>
-                <th class="text-left">
-                    Accompanist
-                </th>
-                <th>
-                    |
-                </th>
-                <th class="text-left">
-                    Student
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="data in props.dataTableData">
-                <td>
-                    {{ data.time.timeText }}
-                </td>
-                <td>
-                    |
-                </td>
-                <td>
-                    <div v-for="faculty in data.faculty">
-                        {{ faculty.firstName + " " + faculty.lastName }}
-                    </div>
-                </td>
-                <td>
-                    |
-                </td>
-                <td>
-                    <div v-for="accompanist in data.accompanist">
-                        {{ accompanist.firstName + " " + accompanist.lastName }}
-                    </div>
-                </td>
-                <td>
-                    |
-                </td>
-                <td>
-                    <div v-for="student in data.student">
-                        {{ student.firstName + " " + student.lastName }}
-                    </div>
-                </td>
-            </tr>
-        </tbody>
-    </v-table>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Time slot</th>
+        <th>|</th>
+        <th class="text-left">Faculty</th>
+        <th>|</th>
+        <th class="text-left">Accompanist</th>
+        <th>|</th>
+        <th class="text-left">Student</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="data in props.dataTableData">
+        <td>
+          {{ data.time.timeText }}
+        </td>
+        <td>|</td>
+        <td>
+          <div v-for="faculty in data.faculty">
+            {{ faculty.firstName + " " + faculty.lastName }}
+          </div>
+        </td>
+        <td>|</td>
+        <td>
+          <div v-for="accompanist in data.accompanist">
+            {{ accompanist.firstName + " " + accompanist.lastName }}
+          </div>
+        </td>
+        <td>|</td>
+        <td>
+          <div v-for="student in data.student">
+            {{ student.firstName + " " + student.lastName }}
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
--- src/components/admin/maintain/events/ViewSignupsDialog.vue
+++ src/components/admin/maintain/events/ViewSignupsDialog.vue
@@ -8,63 +8,80 @@ import {
   get12HourTimeStringFromString,
   get24HourTimeString,
 } from "../../../../composables/timeFormatter";
-import {generateTimeSlots, subtractTimes} from "../../../../composables/timeManipulator.js";
+import {
+  generateTimeSlots,
+  subtractTimes,
+} from "../../../../composables/timeManipulator.js";
 
 import AvailDataTable from "./AvailDataTable.vue";
 
 const tableData = ref([]);
 const props = defineProps({
   eventData: { type: [Object], required: true },
-  availData: {type: [Object], required: true},
-  studentSignupData: {type: [Object], required: true},
+  availData: { type: [Object], required: true },
+  studentSignupData: { type: [Object], required: true },
 });
 
 function sortData() {
   tableData.value = [];
 
-  var timeSlots = generateTimeSlots(props.eventData.startTime, props.eventData.endTime, 30);
+  var timeSlots = generateTimeSlots(
+    props.eventData.startTime,
+    props.eventData.endTime,
+    30
+  );
   timeSlots.pop();
-  timeSlots.forEach(element => {
+  timeSlots.forEach((element) => {
     tableData.value.push({
       time: element,
       faculty: [],
       accompanist: [],
       student: [],
-    })
-  })
+    });
+  });
   var beginIndex = 0;
 
   // Finds the beginning index
-  props.availData.forEach(element => {
-    for (var i = 0; i < timeSlots.length; i++){
-      if (element.startTime == timeSlots[i].time){
+  props.availData.forEach((element) => {
+    for (var i = 0; i < timeSlots.length; i++) {
+      if (element.startTime == timeSlots[i].time) {
         beginIndex = i;
         break;
       }
     }
-    
-    // Gets the number of 30 minute time slots that this element is available for 
-    var numberOfAvailabilities = subtractTimes(element.startTime, element.endTime) / 30;
+
+    // Gets the number of 30 minute time slots that this element is available for
+    var numberOfAvailabilities =
+      subtractTimes(element.startTime, element.endTime) / 30;
     // If the element's role is a faculty member, add their first and last name to the faculty array
-    if (element.userRole.role.role == "Faculty"){
-      for (var i = 0; i < numberOfAvailabilities; i++){
-        tableData.value[beginIndex + i].faculty.push({firstName: element.userRole.user.firstName, lastName: element.userRole.user.lastName });
+    if (element.userRole.role.role == "Faculty") {
+      for (var i = 0; i < numberOfAvailabilities; i++) {
+        tableData.value[beginIndex + i].faculty.push({
+          firstName: element.userRole.user.firstName,
+          lastName: element.userRole.user.lastName,
+        });
       }
     }
     // If the element's role is an accompanist, add their first and last name to the accompanist array
-    else if (element.userRole.role.role == "Accompanist"){
-      for (var i = 0; i < numberOfAvailabilities; i++){
-        tableData.value[beginIndex + i].accompanist.push({firstName: element.userRole.user.firstName, lastName: element.userRole.user.lastName });
+    else if (element.userRole.role.role == "Accompanist") {
+      for (var i = 0; i < numberOfAvailabilities; i++) {
+        tableData.value[beginIndex + i].accompanist.push({
+          firstName: element.userRole.user.firstName,
+          lastName: element.userRole.user.lastName,
+        });
       }
     }
   });
 
   props.studentSignupData.eventSignups.forEach((element) => {
     var placed = false;
-    for (var i = 0; i <  timeSlots.length - 1 && !placed; i++){
-      if (element.startTime < timeSlots[i].time){
+    for (var i = 0; i < timeSlots.length - 1 && !placed; i++) {
+      if (element.startTime < timeSlots[i].time) {
         element.studentInstrumentSignups.forEach((signup) => {
-          tableData.value[i - 1].student.push({firstName: signup.studentInstrument.studentRole.user.firstName, lastName: signup.studentInstrument.studentRole.user.lastName})
+          tableData.value[i - 1].student.push({
+            firstName: signup.studentInstrument.studentRole.user.firstName,
+            lastName: signup.studentInstrument.studentRole.user.lastName,
+          });
         });
         placed = true;
       }
@@ -74,24 +91,22 @@ function sortData() {
 
 onBeforeMount(() => {
   sortData();
-})
-const emits = defineEmits([
-  "closeSignupsDialog"
-]);
+});
+const emits = defineEmits(["closeSignupsDialog"]);
 </script>
 
 <template>
-    <v-card>
-        <v-card-title>
-            <v-row class="pt-0 mt-0">
-            <v-col
-            cols="auto"
-            class="pt-0 mt-0 text-maroon font-weight-bold text-h4"
-            >
-                View Signups
-          </v-col>
-        </v-row>
-        <v-row class="pt-0 mt-0 pl-2">
+  <v-card min-width="800" max-width="1000">
+    <v-card-title>
+      <v-row class="pt-0 mt-0">
+        <v-col
+          cols="auto"
+          class="pt-0 mt-0 text-maroon font-weight-bold text-h4"
+        >
+          View Signups
+        </v-col>
+      </v-row>
+      <v-row class="pt-0 mt-0 pl-2">
         <v-col cols="9" class="pl-1">
           <v-card-subtitle class="font-weight-bold text-h7 text-darkBlue">
             {{ eventData.name }}
@@ -116,23 +131,18 @@ const emits = defineEmits([
       </v-row>
       <v-row class="pt-0 mt-0 pl-2">
         <v-card>
-          <AvailDataTable
-          :data-table-data="tableData"
-          />
+          <AvailDataTable :data-table-data="tableData" />
         </v-card>
       </v-row>
-        </v-card-title>
-        <v-btn
-          flat
-          class="font-weight-semi-bold mt-0 ml-auto mr-4 mb-2 text-none text-white bg-blue flatChipBorder"
-          
-          @click="emits('closeSignupsDialog')"
-        >
-          Cancel
-        </v-btn>
-    </v-card>
+    </v-card-title>
+    <v-btn
+      flat
+      class="font-weight-semi-bold mt-0 ml-auto mr-4 mb-2 text-none text-white bg-blue flatChipBorder"
+      @click="emits('closeSignupsDialog')"
+    >
+      Cancel
+    </v-btn>
+  </v-card>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
