c27fb0b916741619581fb862d6a8f86cddde8bd4
David North <david.north@oc.edu>
2023-12-26 16:00:17 -0600

    Update : support for disabled user login

--- src/components/student/StudentHome.vue
+++ src/components/student/StudentHome.vue
@@ -10,6 +10,7 @@ import EventSignupItem from "./EventSignupItem.vue";
 import UpcomingEventItem from "../UpcomingEventItem.vue";
 import InstrumentItem from "./InstrumentItem.vue";
 import NotificationItem from "../NotificationItem.vue";
+import UserDataService from "../../services/UserDataService";
 
 const loginStore = useLoginStore();
 const studentRoleId = loginStore.currentRole.id;
@@ -20,6 +21,7 @@ const signups = ref([]);
 const upcomingEvents = ref([]);
 const relevantUpcomingEvents = ref([]);
 const addInstrumentDialog = ref(false);
+const activateStudent = ref(false);
 
 async function retrieveData() {
   await UserNotificationDataService.getByUserRole(loginStore.currentRole.id)
@@ -89,6 +91,21 @@ async function retrieveData() {
     });
 }
 
+async function activateUser() {
+  console.log(loginStore.user);
+  await UserDataService.update({
+    id: loginStore.user.userId,
+    status: "Active",
+  })
+    .then(() => {
+      activateStudent.value = false;
+      addInstrumentDialog.value = true;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
 function hasInstrument(instrumentType) {
   var found = false;
   instruments.value.forEach((instrument) => {
@@ -120,6 +137,9 @@ async function getInstruments() {
 
 onMounted(async () => {
   await retrieveData();
+  if (loginStore.user.status === "Disabled") {
+    activateStudent.value = true;
+  }
 });
 </script>
 
@@ -130,7 +150,10 @@ onMounted(async () => {
         <v-col cols="12" lg="3" class="ma-0 pa-4">
           <v-row class="fill-height ma-0">
             <v-col cols="12" class="pa-0 ma-0 pb-4">
-              <v-card class="fill-height mainCardBorder pa-2" style="overflow-y: auto; max-height: 400px; min-height: 400px;">
+              <v-card
+                class="fill-height mainCardBorder pa-2"
+                style="overflow-y: auto; max-height: 400px; min-height: 400px"
+              >
                 <v-card-title
                   class="font-weight-semi-bold text-blue text-h5 pb-0"
                 >
@@ -153,7 +176,10 @@ onMounted(async () => {
               </v-card>
             </v-col>
             <v-col cols="12" class="pa-0 ma-0 pt-4">
-              <v-card class="fill-height mainCardBorder pa-2" style="overflow-y: auto; max-height: 300px; min-height: 300px;">
+              <v-card
+                class="fill-height mainCardBorder pa-2"
+                style="overflow-y: auto; max-height: 300px; min-height: 300px"
+              >
                 <v-row>
                   <v-col cols="auto">
                     <v-card-title
@@ -163,12 +189,15 @@ onMounted(async () => {
                     </v-card-title>
                   </v-col>
                   <v-spacer></v-spacer>
-                  
-                    <v-btn class="mt-2" flat icon @click="addInstrument">
-                      <v-icon class="text-darkBlue" icon="mdi-plus-circle" size="large">
-                      </v-icon>
-                    </v-btn>
-                  
+
+                  <v-btn class="mt-2" flat icon @click="addInstrument">
+                    <v-icon
+                      class="text-darkBlue"
+                      icon="mdi-plus-circle"
+                      size="large"
+                    >
+                    </v-icon>
+                  </v-btn>
                 </v-row>
 
                 <v-card-text>
@@ -183,7 +212,10 @@ onMounted(async () => {
           </v-row>
         </v-col>
         <v-col cols="12" lg="5" class="pa-0 ma-0 pa-4">
-          <v-card class="fill-height mainCardBorder pa-2" style="overflow-y: auto; max-height: 740px; min-height: 740px;">
+          <v-card
+            class="fill-height mainCardBorder pa-2"
+            style="overflow-y: auto; max-height: 740px; min-height: 740px"
+          >
             <v-card-title class="font-weight-semi-bold text-maroon text-h5">
               Events I'm Signed up For
             </v-card-title>
@@ -205,7 +237,10 @@ onMounted(async () => {
           </v-card>
         </v-col>
         <v-col cols="12" lg="4" class="pa-0 ma-0 pa-4">
-          <v-card class="fill-height mainCardBorder pa-2" style="overflow-y: auto; max-height: 740px; min-height: 740px;">
+          <v-card
+            class="fill-height mainCardBorder pa-2"
+            style="overflow-y: auto; max-height: 740px; min-height: 740px"
+          >
             <v-card-title class="font-weight-semi-bold text-orange text-h5">
               Upcoming Events
             </v-card-title>
@@ -225,6 +260,7 @@ onMounted(async () => {
     <v-dialog v-model="addInstrumentDialog" persistent max-width="600px">
       <UserInstrumentDialogBody
         :is-edit="false"
+        :is-student="true"
         :student-instrument-data="{
           id: null,
           status: 'Active',
@@ -243,6 +279,39 @@ onMounted(async () => {
         "
         @closeUserInstrumentDialogEvent="closeAddInstrumentDialog"
       ></UserInstrumentDialogBody>
+    </v-dialog>
+    <v-dialog v-model="activateStudent" persistent max-width="600px">
+      <v-card class="pa-2 bg-lightBlue flatCardBorder">
+        <v-row class="pt-0 mt-0 text-darkblue font-weight-bold text-h4">
+          <v-card-title>
+            Welcome to the Performance Scheduling System!
+          </v-card-title>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            Please activate your account for this semester by adding an
+            instrument, then adding your repertoire and updating your user
+            information for this Semester!
+          </v-card-text>
+        </v-row>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            flat
+            class="font-weight-semi-bold mt-0 ml-4 mr-auto text-none text-white flatChipBorder bg-darkBlue"
+            @click="activateUser()"
+          >
+            Activate
+          </v-btn>
+          <v-btn
+            flat
+            class="font-weight-semi-bold mt-0 ml-4 text-none text-white bg-red flatChipBorder"
+            @click="activateStudent = false"
+          >
+            Don't Activate
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-dialog>
   </div>
 </template>
