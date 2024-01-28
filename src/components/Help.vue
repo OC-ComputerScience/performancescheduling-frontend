e5bccf83126a241ffe1c6eed2063d0e6276de90a
JuliaFlores21 <julia.flores@eagles.oc.edu>
2024-01-28 14:59:01 -0600

    create help screen

---
+++ src/components/Help.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { ref, onMounted } from "vue";
+// import { useLoginStore } from "../stores/LoginStore.js";
+// import { storeToRefs } from "pinia";
+import VuePdfEmbed from 'vue-pdf-embed';
+import 'vue-pdf-embed/dist/style/index.css'
+
+
+const pdfSource = 'https://drive.google.com/file/d/1FrcHfkSbgyX60g5FlLGL8cZyEHiXZwl1/preview'
+
+</script>
+
+<template>
+
+<VuePdfEmbed :source="pdfSource" />
+
+</template>
